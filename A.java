16f989ac2759706ef61f3c083c7d01f3d30b1e03
Ahmet Kılıçarslan <Ahmet.kilicarslan1717@gmail.com>
2025-04-29 12:06:08 +0300

    Add files via upload
    for coding part

---
+++ A.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package a;
+
+/**
+ *
+ * @author FSMVU
+ */
+public class A {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.print("Ahmet");
+    }
+    
+}
---
+++ b.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package a;
+
+/**
+ *
+ * @author FSMVU
+ */
+public class b {
+    
+    int age;
+    String name;
+    
+    public b(int age,String name){
+    this.age=age;
+    this.name=name;
+    
+    }
+   
+    
+}
